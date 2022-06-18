9329cfc57e808a5e05d537d1e4ae1bd219416292
arduano <leonid.shchurov@gmail.com>
2022-06-18 23:09:25 +1000

    Made lifetimes static

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,17 @@ use lazy_static::lazy_static;
 use libloading::{Library, Symbol};
 
 /// The dynamic bindings for KDMAPI
-pub struct KDMAPIBinds<'a> {
-    is_kdmapi_available: Symbol<'a, unsafe extern "C" fn() -> bool>,
-    initialize_kdmapi_stream: Symbol<'a, unsafe extern "C" fn() -> i32>,
-    terminate_kdmapi_stream: Symbol<'a, unsafe extern "C" fn() -> i32>,
-    reset_kdmapi_stream: Symbol<'a, unsafe extern "C" fn()>,
-    send_direct_data: Symbol<'a, unsafe extern "C" fn(u32) -> u32>,
-    send_direct_data_no_buf: Symbol<'a, unsafe extern "C" fn(u32) -> u32>,
+pub struct KDMAPIBinds {
+    is_kdmapi_available: Symbol<'static, unsafe extern "C" fn() -> bool>,
+    initialize_kdmapi_stream: Symbol<'static, unsafe extern "C" fn() -> i32>,
+    terminate_kdmapi_stream: Symbol<'static, unsafe extern "C" fn() -> i32>,
+    reset_kdmapi_stream: Symbol<'static, unsafe extern "C" fn()>,
+    send_direct_data: Symbol<'static, unsafe extern "C" fn(u32) -> u32>,
+    send_direct_data_no_buf: Symbol<'static, unsafe extern "C" fn(u32) -> u32>,
     is_stream_open: AtomicBool,
 }
 
-impl<'a> KDMAPIBinds<'a> {
+impl KDMAPIBinds {
     /// Calls `IsKDMAPIAvailable`
     pub fn is_kdmapi_available(&self) -> bool {
         unsafe { (self.is_kdmapi_available)() }
@@ -26,7 +26,7 @@ impl<'a> KDMAPIBinds<'a> {
     /// Automatically calls `TerminateKDMAPIStream` when dropped.
     ///
     /// Errors if multiple streams are opened in parallel.
-    pub fn open_stream(&'a self) -> KDMAPIStream<'a> {
+    pub fn open_stream(&'static self) -> KDMAPIStream {
         if self
             .is_stream_open
             .load(std::sync::atomic::Ordering::Relaxed)
@@ -47,7 +47,7 @@ fn load_kdmapi_lib() -> Library {
     unsafe { Library::new("OmniMIDI\\OmniMIDI").unwrap() }
 }
 
-fn load_kdmapi_binds<'a>(lib: &'a Library) -> KDMAPIBinds<'a> {
+fn load_kdmapi_binds(lib: &'static Library) -> KDMAPIBinds {
     unsafe {
         KDMAPIBinds {
             is_kdmapi_available: lib.get(b"IsKDMAPIAvailable").unwrap(),
@@ -64,18 +64,18 @@ fn load_kdmapi_binds<'a>(lib: &'a Library) -> KDMAPIBinds<'a> {
 /// Struct that provides access to KDMAPI's stream functions
 ///
 /// Automatically calls `TerminateKDMAPIStream` when dropped.
-pub struct KDMAPIStream<'a> {
-    binds: &'a KDMAPIBinds<'a>,
+pub struct KDMAPIStream {
+    binds: &'static KDMAPIBinds,
 }
 
-impl<'a> KDMAPIStream<'a> {
+impl KDMAPIStream {
     /// Calls `ResetKDMAPIStream`
     pub fn reset(&self) {
         unsafe {
             (self.binds.reset_kdmapi_stream)();
         }
     }
-    
+
     /// Calls `SendDirectData`
     pub fn send_direct_data(&self, data: u32) -> u32 {
         unsafe { (self.binds.send_direct_data)(data) }
@@ -87,7 +87,7 @@ impl<'a> KDMAPIStream<'a> {
     }
 }
 
-impl<'a> Drop for KDMAPIStream<'a> {
+impl Drop for KDMAPIStream {
     fn drop(&mut self) {
         unsafe {
             (self.binds.terminate_kdmapi_stream)();
@@ -100,7 +100,7 @@ impl<'a> Drop for KDMAPIStream<'a> {
 
 lazy_static! {
     static ref KDMAPI_LIB: Library = load_kdmapi_lib();
-    
+
     /// The dynamic library for KDMAPI. Is loaded when this field is accessed.
-    pub static ref KDMAPI: KDMAPIBinds<'static> = load_kdmapi_binds(&KDMAPI_LIB);
+    pub static ref KDMAPI: KDMAPIBinds = load_kdmapi_binds(&KDMAPI_LIB);
 }
